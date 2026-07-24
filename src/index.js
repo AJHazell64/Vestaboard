09b99689e64a3baea663f3fa73e86796a7c0caba
AJHazell64 <adam@haz.one>
2026-07-24 02:04:37 +0100

    Fix night mode display

--- src/index.js
+++ src/index.js
@@ -13,6 +13,6 @@ if (mode === "clock") {
   const { createMorningDisplay } = await import("./morning.js");
   message = await createMorningDisplay();
 } else {
-  message = `MODE: ${mode}`;
+  message = await createMorningDisplay();
 }
 await sendToVestaboard(message);
