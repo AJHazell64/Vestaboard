abdc11bff9fd489f3920dc5884265c94a895162d
AJHazell64 <adam@haz.one>
2026-07-25 14:18:12 +0100

    Add first nature artwork
    Removed multiple artwork entries and added lines to 'Sunrise'.

--- data/artwork.js
+++ data/artwork.js
@@ -3,45 +3,9 @@ export const artwork = [
     name: "Sunrise",
     category: "nature",
     lines: [
-      "",
-      "",
-      "",
-    ],
-  },
-  {
-    name: "Mountains",
-    category: "nature",
-    lines: [
-      "",
-      "",
-      "",
-    ],
-  },
-  {
-    name: "Ocean",
-    category: "nature",
-    lines: [
-      "",
-      "",
-      "",
-    ],
-  },
-  {
-    name: "Forest",
-    category: "nature",
-    lines: [
-      "",
-      "",
-      "",
-    ],
-  },
-  {
-    name: "Moon",
-    category: "nature",
-    lines: [
-      "",
-      "",
-      "",
+      "      YYY      ",
+      "    OYYYYYO    ",
+      "GGGGGGGGGGGGGGG",
     ],
   },
 ];
