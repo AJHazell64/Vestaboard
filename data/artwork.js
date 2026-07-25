2b7f2c84437f1b4174745588e444d02a8c8a44d0
AJHazell64 <adam@haz.one>
2026-07-25 14:06:34 +0100

    Create artwork library framework

--- data/artwork.js
+++ data/artwork.js
@@ -1,1 +1,47 @@
-
+export const artwork = [
+  {
+    name: "Sunrise",
+    category: "nature",
+    lines: [
+      "",
+      "",
+      "",
+    ],
+  },
+  {
+    name: "Mountains",
+    category: "nature",
+    lines: [
+      "",
+      "",
+      "",
+    ],
+  },
+  {
+    name: "Ocean",
+    category: "nature",
+    lines: [
+      "",
+      "",
+      "",
+    ],
+  },
+  {
+    name: "Forest",
+    category: "nature",
+    lines: [
+      "",
+      "",
+      "",
+    ],
+  },
+  {
+    name: "Moon",
+    category: "nature",
+    lines: [
+      "",
+      "",
+      "",
+    ],
+  },
+];
