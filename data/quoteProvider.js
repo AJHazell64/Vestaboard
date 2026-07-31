8eb280185e2d0edd2d2c2acfc36b2f7afec4cb9a
AJHazell64 <adam@haz.one>
2026-07-31 19:41:54 +0100

    Implement non-repeating quote queue

--- data/quoteProvider.js
+++ data/quoteProvider.js
@@ -1,5 +1,43 @@
-import { getFallbackQuote } from "./quotes.js";
+import { getValidFallbackQuotes } from "./quotes.js";
 
-export async function getQuote() {
-  return getFallbackQuote();
+function shuffleQuotes(quotes) {
+  const shuffledQuotes = [...quotes];
+
+  for (
+    let index = shuffledQuotes.length - 1;
+    index > 0;
+    index -= 1
+  ) {
+    const randomIndex =
+      Math.floor(Math.random() * (index + 1));
+
+    [
+      shuffledQuotes[index],
+      shuffledQuotes[randomIndex],
+    ] = [
+      shuffledQuotes[randomIndex],
+      shuffledQuotes[index],
+    ];
+  }
+
+  return shuffledQuotes;
+}
+
+export async function getQuote(savedQueue = []) {
+  let quoteQueue = Array.isArray(savedQueue)
+    ? [...savedQueue]
+    : [];
+
+  if (quoteQueue.length === 0) {
+    quoteQueue = shuffleQuotes(
+      getValidFallbackQuotes()
+    );
+  }
+
+  const quote = quoteQueue.shift();
+
+  return {
+    quote,
+    queue: quoteQueue,
+  };
 }
