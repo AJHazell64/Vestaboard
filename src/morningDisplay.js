b5d9bafdc078f7f688f9e5ef1880508593dc4619
AJHazell64 <adam@haz.one>
2026-08-02 20:23:47 +0100

    Add morning display module

--- src/morningDisplay.js
+++ src/morningDisplay.js
@@ -1,1 +1,12 @@
-
+export function createMorningDisplay(
+  weather,
+  teslaRangeMiles
+) {
+  return {
+    line1: "",
+    line2: "",
+    line3: "",
+    line1Colour: "WHITE",
+    rainBarCells: 0,
+  };
+}
